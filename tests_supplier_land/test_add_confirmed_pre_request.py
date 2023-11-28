094c827e7827415efe2121e9438f54c80c408ac6
Jeremy Marcos <jeremy@compassstarltd.com>
2023-11-28 14:24:33 +0800

    update progress bar in logs

--- tests_supplier_land/test_add_confirmed_pre_request.py
+++ tests_supplier_land/test_add_confirmed_pre_request.py
@@ -4,6 +4,7 @@ from pages_supplier_land.sl_login_page import SupplierLandLoginPage
 import pytest
 
 from utilities.custom_logging import get_custom_logger
+from utilities.loading_bar import updt
 
 logger = get_custom_logger(__name__)
 
@@ -17,6 +18,7 @@ class AddConfirmedPreRequest(
 
     @pytest.mark.run(order=1)
     def test_login(self):
+        updt(4, 1)
         logger.info("Starting the login test as intermediary...")
         self.open_supplier_land_page()
         self.supplier_land_login_as_intermediary()
@@ -24,12 +26,14 @@ class AddConfirmedPreRequest(
 
     @pytest.mark.run(order=2)
     def test_navigate_to_pre_request(self):
+        updt(4, 2)
         logger.info("Navigating to the pre-request page...")
         self.navigate_to_pre_request()
         logger.info("Navigated to pre-request page successfully.")
 
     @pytest.mark.run(order=3)
     def test_pre_request_fields(self):
+        updt(4, 3)
         logger.info("Adding new pre-request fields...")
         self.add_pre_request_fields()
         self.submit_pre_request()
@@ -37,6 +41,7 @@ class AddConfirmedPreRequest(
 
     @pytest.mark.run(order=4)
     def test_search_company_in_offerings(self):
+        updt(4, 3.5)
         logger.info("Logging in as George...")
         self.get_new_driver()
         self.open_supplier_land_page()
@@ -50,3 +55,4 @@ class AddConfirmedPreRequest(
         logger.info("Searching for a company in offerings...")
         self.search_company_in_offerings()
         logger.info("Company search in offerings completed.")
+        updt(4, 4)
