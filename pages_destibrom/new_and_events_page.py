c2372f235cc0d979850f430489dffa83d1b9182c
Jeremy Marcos <jeremy@compassstarltd.com>
2023-11-30 22:31:21 +0800

    updates

--- pages_destibrom/new_and_events_page.py
+++ pages_destibrom/new_and_events_page.py
@@ -1,9 +1,23 @@
 from seleniumbase import BaseCase
 from configuration_files.config_reader import Readconfig
+from utilities.random_website_generator import RandomWebsiteGenerator
 
 
 class DestibromNewsAndEventsPage(BaseCase):
     news_and_events_menu = "//a[contains(.,'News and Events')]"
+    add_news_and_events_button = "//a[contains(.,'Add News and Event')]"
+    news_and_events_content_link = "//input[@id='content_link']"
+    news_and_events_save_button = "//button[.='Save']"
 
     def navigate_to_news_and_events_menu(self):
         self.click(self.news_and_events_menu)
+
+    def add_new_for_news_and_events(self):
+        website_generator = RandomWebsiteGenerator()
+        website_url = website_generator.generate_random_website()
+
+        self.click(self.add_news_and_events_button)
+        self.type(self.news_and_events_content_link, website_url)
+        # self.click(self.news_and_events_save_button)
+
+
--- pages_destibrom/sponsored_ads_page.py
+++ pages_destibrom/sponsored_ads_page.py
@@ -1,9 +1,21 @@
 from seleniumbase import BaseCase
 from configuration_files.config_reader import Readconfig
+from utilities.random_website_generator import RandomWebsiteGenerator
 
 
 class DestibromSponsoredAdsPage(BaseCase):
     sponsored_ads_menu = "//a[contains(.,'Sponsored Ads')]"
+    add_sponsored_ads_button = "//a[contains(.,'Add Sponsored Ad')]"
+    sponsored_ads_content_link = "//input[@id='content_link']"
+    sponsored_ads_button = "//button[.='Save']"
 
     def navigate_to_sponsored_ads_menu(self):
         self.click(self.sponsored_ads_menu)
+
+    def add_new_for_sponsored_ads(self):
+        website_generator = RandomWebsiteGenerator()
+        website_url = website_generator.generate_random_website()
+
+        self.click(self.add_sponsored_ads_button)
+        self.type(self.sponsored_ads_content_link, website_url)
+        # self.click(self.sponsored_ads_button)
