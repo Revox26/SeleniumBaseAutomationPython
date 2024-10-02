d4a9643ebc5f97aa2dc5fb0108d0b0406b360460
Jeremy Marcos <jeremy@compassstarltd.com>
2024-10-02 22:11:53 +0800

    update destination x process

--- AppiumPython/pages_destination_x/create_an_account_screen.py
+++ AppiumPython/pages_destination_x/create_an_account_screen.py
@@ -10,8 +10,8 @@ class DestinationxCreateAnAccountPage(AndroidCapabilities):
     first_name = '//android.widget.EditText[@resource-id="RNE__Input__text-input" and @text="First Name"]'
     surname = '//android.widget.EditText[@resource-id="RNE__Input__text-input" and @text="Surname"]'
     email_address = '//android.widget.EditText[@resource-id="RNE__Input__text-input" and @text="Email Address"]'
-    password = '//android.widget.EditText[@resource-id="RNE__Input__text-input" and @text="Password"]'
-    confirm_password = '//android.widget.EditText[@resource-id="RNE__Input__text-input" and @text="Confirm Password"]'
+    password_ = '//android.widget.EditText[@resource-id="RNE__Input__text-input" and @text="Password"]'
+    confirm_password_ = '//android.widget.EditText[@resource-id="RNE__Input__text-input" and @text="Confirm Password"]'
     sign_up_button = '//android.widget.Button[@content-desc="Signup"]'
     cancel_button = '//android.widget.TextView[@text="Cancel"]'
 
@@ -24,11 +24,11 @@ class DestinationxCreateAnAccountPage(AndroidCapabilities):
         timestamp = datetime.now().strftime("%H%M%S")
         random_email = random_first_name + "" + random_surname + timestamp + "@testing.com"
 
-
+        mobile_action.swipe_down_until_element_is_visible(self.create_an_account_button)
         mobile_action.tap(self.create_an_account_button)
         mobile_action.type_text(self.first_name, random_first_name)
         mobile_action.type_text(self.surname, random_surname)
         mobile_action.type_text(self.email_address, random_email)
-        mobile_action.type_text(self.password, Readconfig.get_director_password())
-        mobile_action.type_text(self.confirm_password, Readconfig.get_director_password())
+        mobile_action.type_text(self.password_, Readconfig.get_director_password())
+        mobile_action.type_text(self.confirm_password_, Readconfig.get_director_password())
         mobile_action.tap(self.sign_up_button)
---
+++ AppiumPython/pages_destination_x/login_screen.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+
+from faker import Faker
+
+from AppiumPython.actions.mobile_actions import MobileCustomActionClass
+from AppiumPython.device_capabilities.android_capabilities import AndroidCapabilities
+
+
+class DestinationxLoginPage(AndroidCapabilities):
+    email = '//android.widget.EditText[@resource-id="email"]'
+    password = '//android.widget.EditText[@resource-id="password"]'
+    destix_login = '//android.widget.Button[@content-desc="Login"]'
+    sign_in_with_google = '//android.widget.TextView[@text="Sign in with Google"]'
+    sign_in_with_apple = '//android.widget.TextView[@text="Sign in with Apple"]'
+    create_an_account = '//android.widget.Button[@content-desc="Create an Account"]'
+    forgot_password = '//android.widget.TextView[@text="Forgot Password?"]'
+    forgot_password_email = '(//android.widget.EditText[@resource-id="email"])[1]'
+    reset_password = '//android.widget.Button[@content-desc="Reset Password"]'
+
+    def navigate_to_sign_in_with_google(self):
+        mobile_action = MobileCustomActionClass()
+        mobile_action.tap(self.sign_in_with_google)
+
+    def navigate_to_sign_in_with_apple(self):
+        mobile_action = MobileCustomActionClass()
+        mobile_action.tap(self.sign_in_with_apple)
+
+    def navigate_to_create_an_account(self):
+        mobile_action = MobileCustomActionClass()
+        mobile_action.tap(self.create_an_account)
+
+    def navigate_to_login(self):
+        mobile_action = MobileCustomActionClass()
+        if mobile_action.assert_element(self.email):
+            pass
+        else:
+            mobile_action.tap(self.destix_login)
+
+    def navigate_to_forgot_password(self):
+        mobile_action = MobileCustomActionClass()
+
+        fake = Faker()
+        random_first_name = fake.first_name()
+        random_surname = fake.last_name()
+        timestamp = datetime.now().strftime("%H%M%S")
+        random_email = random_first_name + "" + random_surname + timestamp + "@testing.com"
+
+        mobile_action.tap(self.forgot_password)
+        mobile_action.assert_element(self.reset_password)
+        mobile_action.type_text(self.forgot_password_email, random_email)
+        mobile_action.tap(self.reset_password)
