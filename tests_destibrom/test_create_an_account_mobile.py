8decb88b2c20307ac99d18940d6990e4ba037e95
Jeremy Marcos <jeremy@compassstarltd.com>
2024-10-02 22:15:05 +0800

    update destination x process

--- tests_destibrom/test_create_an_account_mobile.py
+++ tests_destibrom/test_create_an_account_mobile.py
@@ -1,11 +1,12 @@
 import pytest
 
 from AppiumPython.pages_destination_x.create_an_account_screen import DestinationxCreateAnAccountPage
+from AppiumPython.pages_destination_x.login_screen import DestinationxLoginPage
 from pages_destibrom.login_page import DestiBromLoginPage
 
 
 class DestinationxCreateAnAccountMobile(
-
+    DestinationxLoginPage,
     DestinationxCreateAnAccountPage,
     DestiBromLoginPage
 
@@ -13,4 +14,5 @@ class DestinationxCreateAnAccountMobile(
 
     @pytest.mark.run(order=1)
     def test_create_account_mobile(self):
+        self.navigate_to_login()
         self.register_new_account()
