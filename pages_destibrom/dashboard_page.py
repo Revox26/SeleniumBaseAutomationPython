abc8063079e13c2b5b3ec5763f835e19968d8121
Jeremy Marcos <jeremy@compassstarltd.com>
2024-10-02 22:12:31 +0800

    update destination x process

--- pages_destibrom/dashboard_page.py
+++ pages_destibrom/dashboard_page.py
@@ -12,17 +12,24 @@ class DestibromDashboardPage(BaseCase):
     dashboard_administrator = "//h5[.='  Administrators']"
     dashboard_notification_sent = "//h5[.=' Notifications Sent']"
 
+    firebase_label = "//h5[.='Firebase  ']"
+    sendgrid_label = "// h5[. = 'SendGrid  ']"
+
+
 
 
 
     def navigate_to_dashboard_menu(self):
+
         self.click(self.dashboard_menu)
 
     def choose_a_destination(self):
-        self.assert_element(self.add_new_destination_button)
-        self.assert_element(self.dashboard_location, timeout=60)
-        self.assert_element(self.dashboard_sponsored_ads, timeout=60)
-        self.assert_element(self.dashboard_news_and_events, timeout=60)
-        self.assert_element(self.dashboard_administrator, timeout=60)
-        self.assert_element(self.dashboard_notification_sent, timeout=60)
+        self.assert_element(self.firebase_label, timeout=60)
+        self.assert_element(self.firebase_label, timeout=60)
+        # self.assert_element(self.add_new_destination_button)
+        # self.assert_element(self.dashboard_location, timeout=60)
+        # self.assert_element(self.dashboard_sponsored_ads, timeout=60)
+        # self.assert_element(self.dashboard_news_and_events, timeout=60)
+        # self.assert_element(self.dashboard_administrator, timeout=60)
+        # self.assert_element(self.dashboard_notification_sent, timeout=60)
 
